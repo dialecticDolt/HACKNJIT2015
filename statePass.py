342aa29b76f6f149a820cf368b5dcda72585670f
dialecticDolt <will.ruys@gmail.com>
2015-11-07 16:50:47 -0500

    Almost Working Simple Example

--- statePass.py
+++ statePass.py
@@ -3,46 +3,65 @@
 Test of Netorking Portion of HackNJIT
 @author: William L. Ruys 
 """
+import pickle
 import socket
 import sys
+import threading
 
-class: gameState
-    def update 
-        for e in entList:
-            e.getID()
-            e.getPos()
+isServer = True;
+HOST = 'localhost';
+PORT = 50007;
+stateBuffer = [];
+msg = "";
+currentTurn = 0;
+eList = ["hey"]
+eList.append("who");
+wList = 123456;
+
+class gameState:
+    def __init__(self, eList, wList): 
+        self.turnNumber = currentTurn;    """Attach Turn information"""
+        self.eList = eList;
+        self.wList = wList;   
         
+        
+s = socket.socket();           
+           
+if(isServer== True):
+    try:
+        s.bind((HOST, PORT))
+    except (s.error , msg):
+        print ('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+        sys.exit()
     
+    print ('Socket bind complete')
+    s.listen(1)
+
+    while True:
+        # the s.accept() command returns two variables that we save. client is
+        # the client socket which we can use to send and receive data. the
+        # address is the address bound to the client socket
+        client, addr = s.accept()
+        
+        # print a success message for connection
+        print ('Got connection from', addr)
+       # create our message to send to server
+        msg = 'Server: You are connected'
+
+       # we overwrite the msg variable with a message from the client. the
+       # message is decoded and printed on the server
+        msg = client.recv(1024).decode()
+        msg2 = pickle.loads(msg)
+        print(msg2.wList)
+       
+       # we then close the connection and exit our loop, terminating the
+       # program
+        client.close()
+        break;                   
+else:
+    gState = gameState(eList, wList);
+    print(gState.eList)
+    dataString = pickle.dumps(gState)
+    s.connect((HOST, PORT));
+    s.send(dataString.encode());
     
-    
-class mysocket:
-    '''from python socket website
-    '''
-    def __init__(self, sock=None):
-        if sock is None:
-            self.sock = socket.socket(
-                socket.AF_INET, socket.SOCK_STREAM)
-        else:
-            self.sock = sock
-
-    def connect(self, host, port):
-        self.sock.connect((host, port))
-
-    def mysend(self, msg):
-        totalsent = 0
-        while totalsent < MSGLEN:
-            sent = self.sock.send(msg[totalsent:])
-            if sent == 0:
-                raise RuntimeError("socket connection broken")
-            totalsent = totalsent + sent
-
-    def myreceive(self):
-        chunks = []
-        bytes_recd = 0
-        while bytes_recd < MSGLEN:
-            chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
-            if chunk == '':
-                raise RuntimeError("socket connection broken")
-            chunks.append(chunk)
-            bytes_recd = bytes_recd + len(chunk)
-        return ''.join(chunks)
