e7f7158bbc3bf0c25948a0d7527d6acf3887b9bf
dialecticDolt <will.ruys@gmail.com>
2015-11-07 15:09:14 -0500

    Starting to Make Networking Thing

---
+++ statePass.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Test of Netorking Portion of HackNJIT
+@author: William L. Ruys 
+"""
+
+class: gameState
+    def update 
+        for e in entList:
+            e.getPosx
+        
+    
