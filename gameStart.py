5479ef2e96db1e63b084f43b0a7b6cf0baf9fec0
dialecticDolt <will.ruys@gmail.com>
2015-11-07 23:22:47 -0500

    Some Stuff

--- gameStart.py
+++ gameStart.py
@@ -11,7 +11,7 @@ Initialize Character
 Create Map
 loop:
     Initialize World Entities
-    Wait for Keyboard Input 
+    Wait for Keyboard Input
     Do Actions
     End turn
     if server wait
@@ -24,11 +24,14 @@ import threading
 import socket
 import sys
 import os
-import pickle 
+import pickle
+import pygame
+import pygcurse
+import msvcrt
 import time
 
 endFlag = False;
-isServer = False;
+isServer = True;
 updateFlag = False;
 turnFlag = True;
 multiFlag = False;
@@ -47,7 +50,7 @@ stateBuffer = []; """Store all the turns that have happened locally"""
 
 class gameState:
     """This is what is communicated across the network"""
-    def __init__(self, eList, wList): 
+    def __init__(self, eList, wList):
         self.turnNumber = currentTurn;    """Attach Turn information"""
         self.eList = eList;               """State of all entities"""
         self.wList = wList;               """State of all world things"""
@@ -65,39 +68,77 @@ class NJITHack(threading.Thread):
         global endFlag
         global gState
         global lockTurn
-        """
-        STARTING MENU
-        """
-            
-        """
-        Initialize Character
-        """
-            
-        """
-        Create Map & Initialize World Entities
-        """
-            
-        """Create GameState"""
-        
-        """
-        Start Server Listening
-        -Server starts waiting
-        -Client will send signal if turnFlag is set
-        -Server will send response on its turnFlag & recieve
-        -
-        """
-        
-        """
-        All Actions and End Turn
-        -All interactions
-        -Invetory 
-        -Set End Turn Flag
-        """
- 
+        exitFlag = True;
+        reallyexitFlag = True;
+        State = 1;
+        while reallyexitFlag
+            while exitFlag:
+                """
+                STARTING MENU
+                """
+                if State == 1:
+                    print(" _   _      _ _____ _______   _    _          _____ _  __".center(100));
+                    print("| \ | |    | |_   _|__   __| | |  | |   /\   / ____| |/ /".center(100));
+                    print("|  \| |    | | | |    | |    | |__| |  /  \ | |    | ' / ".center(100));
+                    print("| . ` |_   | | | |    | |    |  __  | / /\ \| |    |  <  ".center(100));
+                    print("| |\  | |__| |_| |_   | |    | |  | |/ ____ \ |____| . \ ".center(100));
+                    print("|_| \_|\____/|_____|  |_|    |_|  |_/_/    \_\_____|_|\_\ ".center(100));
+                    print("==========================================================".center(100));
+                    print("Created by Josef Mohrenweiser, Tyler Shuhnicki, and William Ruys".center(100));
+                    print("****Single-Player".center(100));
+                    print("----Multi-Player".center(100));
+                if State == 2:
+                    print(" _   _      _ _____ _______   _    _          _____ _  __".center(100));
+                    print("| \ | |    | |_   _|__   __| | |  | |   /\   / ____| |/ /".center(100));
+                    print("|  \| |    | | | |    | |    | |__| |  /  \ | |    | ' / ".center(100));
+                    print("| . ` |_   | | | |    | |    |  __  | / /\ \| |    |  <  ".center(100));
+                    print("| |\  | |__| |_| |_   | |    | |  | |/ ____ \ |____| . \ ".center(100));
+                    print("|_| \_|\____/|_____|  |_|    |_|  |_/_/    \_\_____|_|\_\ ".center(100));
+                    print("==========================================================".center(100));
+                    print("Created by Josef Mohrenweiser, Tyler Shuhnicki, and William Ruys".center(100));
+                    print("----Single-Player".center(100));
+                    print("****Multi-Player".center(100));
+                
+                if State == 3:
+                    name = input("Enter your Name".center(100));
+
+
+
+
+
+            """
+            Initialize Character
+            """
+
+            """
+            Create Map & Initialize World Entities
+            """
+
+            """Create GameState"""
+
+            """
+            Start Server Listening
+            -Server starts waiting
+            -Client will send signal if turnFlag is set
+            -Server will send response on its turnFlag & recieve
+            -
+            """
+
+            """
+            All Actions and End Turn
+            -All interactions
+            -Invetory
+            -Set End Turn Flag
+            """
+            #lockEnd.acquire()
+            endFlag = 1;
+            #lockEnd.release();
+        return;
+
 class Network(threading.Thread):
     def __init__(self):
         super(Network, self).__init__()
-        
+
     def run(self):
         global turnFlag
         global stateBuffer
@@ -108,8 +149,8 @@ class Network(threading.Thread):
         global lockEnd
        # msg = "";
         s = socket.socket();
-        dataString = pickle.dumps(gState);   #what ever state to send    
-        
+        dataString = pickle.dumps(gState);   #what ever state to send
+
         if isServer:
             #try:
             s.bind(("", PORT))
@@ -124,37 +165,37 @@ class Network(threading.Thread):
                 stateBuffer.append(pickle.loads(pickledState));
                 lockTurn.acquire();
                 if turnFlag:
+                    lockState.acquire();
                     tempState = stateBuffer.pop();
                     """tempState = tempState.update();"""
                     dataString = pickle.dumps(tempState)
                     client.send(dataString.encode())
-                lockTurn.release(); 
-                lockEnd.acquire();
+                    lockState.release();
+                lockTurn.release();
                 if endFlag:
-                    lockEnd.release();
                     client.close();
                     break;
-                lockEnd.release();
-                   
+
         else:
-            
+
             s.connect((HOST, PORT));
             while True:
                 lockTurn.acquire();
                 if turnFlag:
+                    lockState.acquire()
                     s.send(dataString.encode());
                     pickledState = s.recv(1024).decode();
                     stateBuffer.append(pickle.loads(pickledState));
                     turnFlag = False;
+                    lockState.release();
                 lockTurn.release();
-                lockEnd.acquire();
                 if endFlag:
-                    lockEnd.release();
-                    break;   
-                lockEnd.release();
-                        
+                    break;
+                return;
+
 thread1 = NJITHack();
 thread2 = Network();
 thread1.start();
-thread2.start();       
-        
+thread2.start();
+#print("Done")
+sys.exit();
